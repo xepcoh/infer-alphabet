faf0eb2f232897360e333ddf735306571c490546
Dima Kherson <GitHub@dimux.net>
2011-11-30 14:57:00 -0600

    Fixed the left-behind discern() ref in show-alphabet.rb.

--- show-alphabet.rb
+++ show-alphabet.rb
@@ -3,7 +3,7 @@ require 'alphabet'
 a = Alphabet.new
 a.from_file("./alphabet.txt", "r")
 i = 1
-a.discern.each do |letter| 
+a.deduce.each do |letter| 
 	puts "Letter #{i} is #{letter}"
 	i += 1
 end
